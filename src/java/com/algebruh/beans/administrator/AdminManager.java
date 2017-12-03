7536935fc56cd572529749197a8001b2b41a458d
Armand Tavera <yosoy_superyoyo@hotmail.com>
2017-12-02 23:52:10 -0600

    Modulo de admin en trabajo, problema llenando List para XHTML.
    Tabla semi-creada.
    Errores en Sesión

---
+++ src/java/com/algebruh/beans/administrator/AdminManager.java
@@ -0,0 +1,75 @@
+package com.algebruh.beans.administrator;
+import com.algebruh.common.utils.UserType;
+import entity.HibernateUtil;
+import entity.User;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import static java.lang.Boolean.TRUE;
+import java.util.Iterator;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import org.hibernate.Query;
+import org.hibernate.Session;
+@Named(value = "adminManager")
+@SessionScoped
+public class AdminManager implements Serializable {
+    private FacesContext fc;
+    private HttpServletRequest request;
+    private FacesMessage fm;
+    private Session hibernateSession;
+    private AdminTable  adminSet;
+    private List<AdminTable> adminTable;
+    
+    public AdminManager() {
+        List<User> userList;
+        UserType type;
+        
+        hibernateSession = HibernateUtil.getSessionFactory().openSession();
+        hibernateSession.beginTransaction();
+        fc= FacesContext.getCurrentInstance();
+        request = (HttpServletRequest)fc.getExternalContext().getRequest();
+        Query users = hibernateSession.createQuery("from User");
+        userList = users.list();
+        System.out.println("query completa " +userList.size());
+        for (User user : userList) {
+            System.out.println("entramos a for "+userList.indexOf(user));
+            if(user != null){
+                type = UserType.UNKNOWN;
+                System.out.println("Usuario Nulo");
+                if(user.getStudent() != null){
+                    System.out.println("Usuario Estudiante");
+                    type = UserType.STUDENT;
+                }else if(user.getTeacher() != null){
+                    System.out.println("Usuario Maestro");
+                    type = UserType.TEACHER;
+                }
+                else if(user.getAdministrator() != null){
+                    System.out.println("Usuario Admin");
+                    type = UserType.ADMINISTRATOR;
+                }
+                System.out.println(user.getFirstnames());
+                System.out.println(user.getSurnames());
+                System.out.println(user.getUsername());
+                System.out.println(user.getPassword());
+                System.out.println(type.name().toLowerCase());
+                System.out.println("Empezamos el adminset");
+                adminSet = new AdminTable(user.getFirstnames(), user.getSurnames(), user.getUsername(), user.getPassword(), type.name().toLowerCase(), TRUE);
+                System.out.println("Terminamos el adminset");
+                adminTable.add(adminSet);
+                System.out.println("agregamos el adminset");
+            }
+        }
+    }
+
+    public List<AdminTable> getAdminTable() {
+        return adminTable;
+    }
+
+    public void setAdminTable(List<AdminTable> adminTable) {
+        this.adminTable = adminTable;
+    }
+    
+}
