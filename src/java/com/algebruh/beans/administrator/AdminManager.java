97e774b2870e5aeb622ec126860c79a4c60f33a5
Armand Tavera <yosoy_superyoyo@hotmail.com>
2017-12-04 22:20:18 -0600

    Modulo de admin en trabajo. AJAX funcional DELETE completo. EDIT completo. ADD ERROR no muestra inputboxes. la variable at no cambia por los inputtexts
    Tabla semi-creada.

--- src/java/com/algebruh/beans/administrator/AdminManager.java
+++ src/java/com/algebruh/beans/administrator/AdminManager.java
@@ -24,17 +24,18 @@ public class AdminManager implements Serializable {
     private HttpServletRequest request;
     private FacesMessage fm;
     private Session hibernateSession;
-    private AdminTable  adminSet;
-    private List<AdminTable> adminTable;
     private Transaction t;
+    
+    private AdminTable  adminSet,newUser;
+    private List<AdminTable> adminTable;
     private List<User> userList;
     private UserType type;
+    private Boolean isAdding;
     
     public AdminManager() {
         adminTable = new ArrayList<>();
-        
+        isAdding = false;
         hibernateSession = HibernateUtil.getSessionFactory().openSession();
-        t= hibernateSession.beginTransaction();
         fc= FacesContext.getCurrentInstance();
         request = (HttpServletRequest)fc.getExternalContext().getRequest();
         Query users = hibernateSession.createQuery("from User");
@@ -106,6 +107,7 @@ public class AdminManager implements Serializable {
         }
     }
     public void deleteUser(int iduser){
+        t= hibernateSession.beginTransaction();
         System.out.println("entramos delete");
         User user = (User) hibernateSession.get(User.class, iduser);
         System.out.println("conseguimos al usuario "+user.getFirstnames());
@@ -125,6 +127,7 @@ public class AdminManager implements Serializable {
         resetAdminTable(); 
     }
     public void saveUser(AdminTable at){
+        t= hibernateSession.beginTransaction();
         System.out.println("entramos al SAVE");
         System.out.println("Nombre editado: "+at.getFirstnames());
         System.out.println("apellido editado: "+at.getSurnames());
@@ -133,23 +136,23 @@ public class AdminManager implements Serializable {
         System.out.println("tipo editado: "+at.getType());
         User user = (User) hibernateSession.get(User.class, at.getIduser());
         System.out.println("conseguimos al usuario "+user.getFirstnames());
+       deleteUserType(user);
         user.setFirstnames(at.getFirstnames());
         user.setSurnames(at.getSurnames());
         user.setPassword(at.getPassword());
         user.setUsername(at.getUsername());
         System.out.println("El usuario es ahora "+user.getFirstnames());
-        hibernateSession.save(user);
         user.setFirstnames(at.getFirstnames());
         user.setSurnames(at.getSurnames());
         user.setPassword(at.getPassword());
         user.setUsername(at.getUsername());
-        System.out.println("Usuario Guardado");
         System.out.println("El usuario es ahora "+user.getFirstnames());
         System.out.println("Es de tipo "+at.getType());
         switch(at.getType()){
             case "1":
                 System.out.println("El usuario es Administrador");
                 Administrator admin = new Administrator(user);
+                user.setAdministrator(admin);
                 hibernateSession.save(admin);
                 System.out.println("Tipo de usuario guardado");
                 break;
@@ -157,6 +160,7 @@ public class AdminManager implements Serializable {
                 System.out.println("El usuario es Profesor");
                 type = UserType.TEACHER;
                 Teacher teach = new Teacher(user);
+                user.setTeacher(teach);
                 hibernateSession.save(teach);
                 System.out.println("Tipo de usuario guardado");
                 break;
@@ -164,6 +168,7 @@ public class AdminManager implements Serializable {
                 System.out.println("El usuario es Estudiante");
                 type = UserType.STUDENT;
                 Student student = new Student(user);
+                user.setStudent(student);
                 hibernateSession.save(student);
                 System.out.println("Tipo de usuario guardado");
                 break;
@@ -174,9 +179,44 @@ public class AdminManager implements Serializable {
                 type = UserType.UNKNOWN;
                 break;
         }
+        hibernateSession.save(user);
+        System.out.println("Usuario Guardado");
         t.commit();
         resetAdminTable();
     }
+    
+    public void deleteUserType(User user){
+        Boolean hasType = true;
+        while(hasType){
+            if(user != null){
+                type = UserType.UNKNOWN;
+                if(user.getStudent() != null){
+                    System.out.println("Usuario Estudiante");
+                    hibernateSession.delete(user.getStudent());
+                    user.setStudent(null);
+                    System.out.println("Ya no es estudiante");
+                    type = UserType.STUDENT;
+                }else if(user.getTeacher() != null){
+                    System.out.println("Usuario Maestro");
+                    type = UserType.TEACHER;
+                    hibernateSession.delete(user.getTeacher());
+                    user.setTeacher(null);
+                    System.out.println("Ya no es maestro");
+                }
+                else if(user.getAdministrator() != null){
+                    System.out.println("Usuario Admin");
+                    hibernateSession.delete(user.getAdministrator());
+                    user.setAdministrator(null);
+                    type = UserType.ADMINISTRATOR;
+                    System.out.println("Ya no es admin");
+                }
+                else{
+                hasType = false;
+                }
+            }
+        }
+    }
+        
     public List<AdminTable> getAdminTable() {
         return adminTable;
     }
@@ -184,4 +224,21 @@ public class AdminManager implements Serializable {
     public void setAdminTable(List<AdminTable> adminTable) {
         this.adminTable = adminTable;
     }
+
+    public AdminTable getNewUser() {
+        return newUser;
+    }
+
+    public void setNewUser(AdminTable newUser) {
+        this.newUser = newUser;
+    }
+    
+    public Boolean getIsAdding() {
+        return isAdding;
+    }
+
+    public void setIsAdding(Boolean isAdding) {
+        this.isAdding = isAdding;
+    }
+    
 }
