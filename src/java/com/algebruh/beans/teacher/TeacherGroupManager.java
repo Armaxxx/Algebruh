059ed936e24141f5c296db14bc2bd7bd52b73f93
Armand Tavera <yosoy_superyoyo@hotmail.com>
2017-12-11 15:30:10 -0600

    Aun no muestra tabla de estudiantes Error al intentar agregar ejercicios Muestra tabla de ejercicios.

--- src/java/com/algebruh/beans/teacher/TeacherGroupManager.java
+++ src/java/com/algebruh/beans/teacher/TeacherGroupManager.java
@@ -29,7 +29,7 @@ public class TeacherGroupManager {
     private final int idGroup;
     private final Schoolgroup group;
     private Exercise newExercise;
-    private Student newStudent;
+    private int newStudent;
     private List<Student> students = new ArrayList<>();
     private List<Student> allStudents = new ArrayList<>();
     private List<Exercise> exercises = new ArrayList<>();
@@ -45,33 +45,62 @@ public class TeacherGroupManager {
         httpSession = request.getSession();
         
         newExercise = new Exercise();
-        newStudent = new Student();
         idGroup = (int)httpSession.getAttribute("id");
         group = (Schoolgroup)hibernateSession.get(Schoolgroup.class, idGroup);
         hasStudents = students.addAll(group.getStudents());
         hasExercises = exercises.addAll(group.getExercises());
         addingExercise = false;
+        addingStudent = false;
         Query query = hibernateSession.createQuery("From Student");
         allStudents = query.list();
         allStudents.removeAll(students);
     }
     
     public void saveNewExercise(){
+        System.out.println("Entramos save exercise");
         Exercise exercise = new Exercise(newExercise.getEquation(), newExercise.getEqtype(), newExercise.getSolution());
         t = hibernateSession.beginTransaction();
+        System.out.println("se crea exercise nuevo "+exercise.getEqtype()+" de "+exercise.getEquation());
         hibernateSession.save(exercise);
+        System.out.println("Lo guardamos");
         group.getExercises().add(exercise);
+        System.out.println("se agrega al grupo");
         hibernateSession.saveOrUpdate(group);
+        System.out.println("se guarda el grupo");
+        exercises.add(exercise);
         t.commit();
     }
-    public void saveNewStudent(){
+    public void saveNewStudent(int newStudent){
+        System.out.println("Entra Save Student");
+        System.out.println("Id de usuario "+newStudent);
+        Student student = (Student)hibernateSession.get(Student.class,newStudent);
         t = hibernateSession.beginTransaction();
-        Student student = (Student)hibernateSession.get(Student.class, newStudent.getIduser());
+        System.out.println("Obtenemos a "+student.getUser().getFirstnames());
         group.getStudents().add(student);
-        hibernateSession.save(group);
-        student.setSchoolgroup(group);
+        System.out.println("Agregamos al estudiante al grupo");
         hibernateSession.saveOrUpdate(group);
+        System.out.println("Guardamos grupo");
+        student.setSchoolgroup(group);
+        System.out.println("Le asignamos grupo al estudiante");
+        hibernateSession.saveOrUpdate(student);
+        System.out.println("Guardamos estudiante");
+        students.add(student);
+        System.out.println("Agregamos al array");
+        hasStudents = true;
         t.commit();
+    }
+    public void deleteUser(int id){
+        t = hibernateSession.beginTransaction();
+        Student student = (Student)hibernateSession.get(Student.class,id);
+        group.getStudents().remove(student);
+        student.setSchoolgroup(null);
+        hibernateSession.saveOrUpdate(group);
+        hibernateSession.saveOrUpdate(student);
+        students.remove(student);
+        if (students.isEmpty())
+            hasStudents = false;
+        t.commit();
+        
     }
     public int getIdGroup() {
         return idGroup;
@@ -117,10 +146,14 @@ public class TeacherGroupManager {
         this.addingStudent = addingStudent;
     }
 
-    public Student getNewStudent() {
+    public int getNewStudent() {
         return newStudent;
     }
 
+    public void setNewStudent(int newStudent) {
+        this.newStudent = newStudent;
+    }
+    
     public List<Student> getAllStudents() {
         return allStudents;
     }
